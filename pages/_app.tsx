446dd73b62159a4794d3a35d91a572cca734d9cd
Dikih Arif Wibowo <dikih.wibowo@dana.id>
2022-12-14 15:46:03 +0700

    feat: add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
+import { Open_Sans } from '@next/font/google'
+
+const openSans = Open_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps}/>
-    </Layout>
+    <>
+      <style jsx global>{`
+        html, body {
+          font-family: ${openSans.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps}/>
+      </Layout>
+    </>
   )
 }
