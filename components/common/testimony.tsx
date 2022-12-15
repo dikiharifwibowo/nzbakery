cf53792b6d434cc2a0558d60b4e0fd2a08f386e6
Dikih Arif Wibowo <dikih.wibowo@dana.id>
2022-12-15 15:25:07 +0700

    feat: add testimony whit slider

--- components/common/testimony.tsx
+++ components/common/testimony.tsx
@@ -1,8 +1,31 @@
-import styles from '../../styles/common/testimony.module.scss';
 import Image from 'next/image';
 import BannerImg from '../../public/banner.png'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from '../../styles/common/testimony.module.scss';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  speed: 200,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
 
 export default function Testimony() {
+
   return (
     <div className={styles.container}>
       <div className={styles.banner}>
@@ -13,59 +36,113 @@ export default function Testimony() {
       </div>
       <div className={styles.content}>
         <div className={styles.title}>Testimoni Kami</div>
-        <figure className={styles.testimony}>
-          <figcaption className={styles.header}>
-            <Image
-              alt='IMG Avatar'
-              width={56}
-              height={56}
-              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
-            />
-            <div>
-              <div className={styles.name}>Debbie O`brien</div>
-              <div className={styles.role}>Senior Software Engineer at Google</div>
-            </div>
-          </figcaption>
-          <blockquote className={styles.caption}>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
-          </blockquote>
-        </figure>
+        <Slider {...settings} className="custom-slick">
+          <figure className={styles.testimony}>
+            <figcaption className={styles.header}>
+              <Image
+                alt='IMG Avatar'
+                width={56}
+                height={56}
+                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+              />
+              <div>
+                <div className={styles.name}>Debbie O`brien</div>
+                <div className={styles.role}>Senior Software Engineer at Google</div>
+              </div>
+            </figcaption>
+            <blockquote className={styles.caption}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+            </blockquote>
+          </figure>
 
-        <figure className={styles.testimony}>
-          <figcaption className={styles.header}>
-            <Image
-              alt='IMG Avatar'
-              width={56}
-              height={56}
-              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
-            />
-            <div>
-              <div className={styles.name}>Debbie O`brien</div>
-              <div className={styles.role}>Senior Software Engineer at Google</div>
-            </div>
-          </figcaption>
-          <blockquote className={styles.caption}>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
-          </blockquote>
-        </figure>
+          <figure className={styles.testimony}>
+            <figcaption className={styles.header}>
+              <Image
+                alt='IMG Avatar'
+                width={56}
+                height={56}
+                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+              />
+              <div>
+                <div className={styles.name}>Debbie O`brien</div>
+                <div className={styles.role}>Senior Software Engineer at Google</div>
+              </div>
+            </figcaption>
+            <blockquote className={styles.caption}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+            </blockquote>
+          </figure>
 
-        <figure className={styles.testimony}>
-          <figcaption className={styles.header}>
-            <Image
-              alt='IMG Avatar'
-              width={56}
-              height={56}
-              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
-            />
-            <div>
-              <div className={styles.name}>Debbie O`brien</div>
-              <div className={styles.role}>Senior Software Engineer at Google</div>
-            </div>
-          </figcaption>
-          <blockquote className={styles.caption}>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
-          </blockquote>
-        </figure>
+          <figure className={styles.testimony}>
+            <figcaption className={styles.header}>
+              <Image
+                alt='IMG Avatar'
+                width={56}
+                height={56}
+                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+              />
+              <div>
+                <div className={styles.name}>Debbie O`brien</div>
+                <div className={styles.role}>Senior Software Engineer at Google</div>
+              </div>
+            </figcaption>
+            <blockquote className={styles.caption}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+            </blockquote>
+          </figure>
+
+          <figure className={styles.testimony}>
+            <figcaption className={styles.header}>
+              <Image
+                alt='IMG Avatar'
+                width={56}
+                height={56}
+                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+              />
+              <div>
+                <div className={styles.name}>Debbie O`brien</div>
+                <div className={styles.role}>Senior Software Engineer at Google</div>
+              </div>
+            </figcaption>
+            <blockquote className={styles.caption}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+            </blockquote>
+          </figure>
+          <figure className={styles.testimony}>
+            <figcaption className={styles.header}>
+              <Image
+                alt='IMG Avatar'
+                width={56}
+                height={56}
+                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+              />
+              <div>
+                <div className={styles.name}>Debbie O`brien</div>
+                <div className={styles.role}>Senior Software Engineer at Google</div>
+              </div>
+            </figcaption>
+            <blockquote className={styles.caption}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+            </blockquote>
+          </figure>
+          <figure className={styles.testimony}>
+            <figcaption className={styles.header}>
+              <Image
+                alt='IMG Avatar'
+                width={56}
+                height={56}
+                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+              />
+              <div>
+                <div className={styles.name}>Debbie O`brien</div>
+                <div className={styles.role}>Senior Software Engineer at Google</div>
+              </div>
+            </figcaption>
+            <blockquote className={styles.caption}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+            </blockquote>
+          </figure>
+        </Slider>
       </div>
     </div>
   )
