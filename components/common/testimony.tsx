013e9f7021adbf28df275d5140dcac4a1b151fb5
Dikih Arif Wibowo <dikih.wibowo@dana.id>
2022-12-15 13:44:27 +0700

    feat: update styles

--- components/common/testimony.tsx
+++ components/common/testimony.tsx
@@ -1,29 +1,72 @@
 import styles from '../../styles/common/testimony.module.scss';
 import Image from 'next/image';
+import BannerImg from '../../public/banner.png'
 
 export default function Testimony() {
   return (
     <div className={styles.container}>
       <div className={styles.banner}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque ipsa quod animi dolore obcaecati sapiente laboriosam corporis minus possimus, fuga veritatis molestias. Rem officia id iusto, sed praesentium expedita ut!
+        <Image
+          src={BannerImg}
+          alt="Banner"
+        />
       </div>
-      <figure className={styles.testimony}>
-        <figcaption className={styles.header}>
-          <Image
-            alt='IMG Avatar'
-            width={56}
-            height={56}
-            src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
-          />
-          <div>
-            <div className={styles.title}>Debbie O`brien</div>
-            <div className={styles.role}>Senior Software Engineer at Google</div>
-          </div>
-        </figcaption>
-        <blockquote className={styles.caption}>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
-        </blockquote>
-      </figure>
+      <div className={styles.content}>
+        <div className={styles.title}>Testimoni Kami</div>
+        <figure className={styles.testimony}>
+          <figcaption className={styles.header}>
+            <Image
+              alt='IMG Avatar'
+              width={56}
+              height={56}
+              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+            />
+            <div>
+              <div className={styles.name}>Debbie O`brien</div>
+              <div className={styles.role}>Senior Software Engineer at Google</div>
+            </div>
+          </figcaption>
+          <blockquote className={styles.caption}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+          </blockquote>
+        </figure>
+
+        <figure className={styles.testimony}>
+          <figcaption className={styles.header}>
+            <Image
+              alt='IMG Avatar'
+              width={56}
+              height={56}
+              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+            />
+            <div>
+              <div className={styles.name}>Debbie O`brien</div>
+              <div className={styles.role}>Senior Software Engineer at Google</div>
+            </div>
+          </figcaption>
+          <blockquote className={styles.caption}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+          </blockquote>
+        </figure>
+
+        <figure className={styles.testimony}>
+          <figcaption className={styles.header}>
+            <Image
+              alt='IMG Avatar'
+              width={56}
+              height={56}
+              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg'
+            />
+            <div>
+              <div className={styles.name}>Debbie O`brien</div>
+              <div className={styles.role}>Senior Software Engineer at Google</div>
+            </div>
+          </figcaption>
+          <blockquote className={styles.caption}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, totam earum tempore esse minus at cumque fugiat, et pariatur beatae modi, nesciunt ut ex facilis fuga? Architecto iusto nemo dolores.
+          </blockquote>
+        </figure>
+      </div>
     </div>
   )
 }
