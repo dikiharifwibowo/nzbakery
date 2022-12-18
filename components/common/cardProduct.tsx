043e7dea1536c2ac4cdf84daaace943084274a3f
Dikih Arif Wibowo <dikih.wibowo@dana.id>
2022-12-18 12:52:28 +0700

    feat: change asset image list products

--- components/common/cardProduct.tsx
+++ components/common/cardProduct.tsx
@@ -1,14 +1,14 @@
 import styles from '../../styles/common/product.module.scss'
 import Image from 'next/image'
-import product1 from '../../public/product-1.png'
 import iconWa from '../../public/icons-whatsapp.gif'
 
-export default function CardProduct() {
+export default function CardProduct(props: string) {
+
   return (
     <div className={styles.card}>
       <Image
         className={styles.image}
-        src={product1}
+        src={`/products/${props.img}`}
         width={400}
         height={400}
         alt='product 1 NZ Bakery'
--- components/common/listProduct.tsx
+++ components/common/listProduct.tsx
@@ -2,13 +2,24 @@ import styles from '../../styles/common/list-product.module.scss'
 import CardProduct from './cardProduct'
 
 export default function ListProduct() {
+  const products = [
+    'product-1.png',
+    'product-2.png',
+    'product-3.png',
+    'product-4.png',
+    'product-5.png',
+    'product-6.png',
+  ]
+
+  const ListProducts = products.map((product, index) => {
+    return <CardProduct key={index} img={product} />
+  })
+
   return (
     <>
       <div className={styles.title}>Produk Kami</div>
       <div className={styles.wrapper}>
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
+        {ListProducts}
       </div>
     </>
   )
